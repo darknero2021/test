3cef59e7b5115fa89c67e81a39155d9e407456be
darknero2021 <darknero@naver.com>
2021-09-22 21:51:14 +0900

    test2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      테스트
+      테스트1
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
